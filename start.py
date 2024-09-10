d18a31a71072ec158cc5345ac58750bd2afada36
Marc Mailloux <marctheshark333@gmail.com>
2024-09-09 20:34:04 -0600

    limiting service test

--- start.py
+++ start.py
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-cat << "EOF"
+import yaml
+import os
+import subprocess
+
+def print_banner():
+    banner = '''
  ______     __     ______     __    __     ______     __   __     ______     __  __     ______  
 /\  ___\   /\ \   /\  ___\   /\ "-./  \   /\  __ \   /\ "-.\ \   /\  __ \   /\ \/\ \   /\__  _\ 
 \ \___  \  \ \ \  \ \ \__ \  \ \ \-./\ \  \ \  __ \  \ \ \-.  \  \ \  __ \  \ \ \_\ \  \/_/\ \/ 
@@ -17,8 +22,35 @@ cat << "EOF"
 /\  ___\   /\  __ \   /\  == \   /\  ___\                                                        
 \ \ \____  \ \ \/\ \  \ \  __<   \ \  __\                                                        
  \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\                                                      
-  \/_____/   \/_____/   \/_/ /_/   \/_____/                                                      
-EOF
+  \/_____/   \/_____/   \/_/ /_/   \/_____/                                        
+'''
+    print(banner)
 
-# Your container's main process goes here
-exec "$@"
+def read_config():
+    with open('conf/conf.yaml', 'r') as file:
+        return yaml.safe_load(file)
+
+def start_services(config):
+    services = config.get('services', {})
+    
+    # Always start the database
+    subprocess.run(['docker-compose', 'up', '-d', 'ui-db'])
+    
+    for service, enabled in services.items():
+        if enabled:
+            print(f"Starting {service} service...")
+            subprocess.run(['docker-compose', 'up', '-d', service])
+        else:
+            print(f"{service} service is disabled in configuration.")
+
+    # Always start nginx last
+    if services.get('nginx', True):  # Default to True if not specified
+        print("Starting nginx service...")
+        subprocess.run(['docker-compose', 'up', '-d', 'nginx'])
+
+if __name__ == "__main__":
+    print_banner()
+    print("Starting Sigmanaut Node Core services...")
+    config = read_config()
+    start_services(config)
+    print("Startup complete.")
